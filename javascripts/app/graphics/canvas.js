08b9ff04a40344d8d2007c01600a959eb409d08e
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-04-09 11:00:34 -0700

    drawing a red square on canvas

---
+++ javascripts/app/graphics/canvas.js
@@ -0,0 +1,29 @@
+window.requestAnimFrame = (function () {
+	return window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	function (callback, element) {
+		window.setTimeout(callback, 1000 / 60);
+	};
+})();
+
+var Canvas = function(id) {
+	this.canvas = document.getElementById("canvas");
+	this.context = this.canvas.getContext("2d");
+	this.width = this.canvas.width;
+	this.height = this.canvas.height;
+
+	this.clear = function(hex) {
+		var color = hex ? hex : "#FFFFFF";
+		this.context.fillStyle = color;
+		this.context.fillRect(0, 0, this.width, this.height);
+	}
+
+	this.draw = function(callback) {
+		callback.call();
+	}
+
+	this.clear();
+}
--- javascripts/app/main.js
+++ javascripts/app/main.js
@@ -4,14 +4,24 @@ var baseUrl = "javascripts/app";
 var includes = [];
 
 include("util", ["prototypes", "logging"]);
+include("graphics", ["canvas"]);
 include("entity", ["entity", "something"]);
 
+var canvas;
 
 function main() {
 	var entity = new Entity(1, 2);
 	var something = new Something(4, 5, 3);
+
 	log(entity);
 	log(something);
+
+	canvas = new Canvas("canvas");
+
+	canvas.draw(function() {
+		canvas.context.fillStyle = "#FF0000";
+		canvas.context.fillRect(canvas.width / 2 - 25, canvas.height / 2 - 25, 50, 50);
+	});
 }
 
 
