9b902813da6d283f6425b153a4fdef6dccbbd141
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-04-17 08:18:43 -0700

    Added enemy queue

--- javascripts/app/engine/enemyqueue.js
+++ javascripts/app/engine/enemyqueue.js
@@ -1,9 +1,22 @@
-var EnemyQueue = function(path) {
+var EnemyQueue = function(enemies, path, rate) {
+	this.enemies = Arrays.isArray(enemies) ? enemies : EnemyQueue.generateEnemies();
 	this.path = path ? path : EnemyQueue.generatePath();
+	this.rate = rate ? rate : 1;
 
+	this.nextEnemy = function() {
+		return enemies.length > 0 enemies.shift() : null;
+	}
+
+	this.getRemainingEnemies = function() {
+		return enemies.length;
+	}
 }
-
 
 EnemyQueue.generatePath = function() {
 
 }
+
+EnemyQueue.generateEnemies = function() {
+
+}
+
