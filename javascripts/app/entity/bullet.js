038bee9a5517afc571872c9311e191bcc9fcf382
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-04-22 00:41:58 -0700

    bullet stub

---
+++ javascripts/app/entity/bullet.js
@@ -0,0 +1,32 @@
+var Bullet = function (x, y, width, height, speed) {
+	Entity.call(this, x, y, width, height);
+	this.pointer = 0;
+	this.pathDrawn = false;
+
+	this.bezierPoints = Bezier.calculateCurve(EnemyQueue.generatePath());
+
+	this.renderPath = function () {
+		this.bezierPoints.forEach(function (p) {
+			gameCanvas.context.fillStyle = "#FF0000";
+			gameCanvas.context.fillRect(p.x, p.y, 1, 1);
+		});
+	};
+
+	this.render = function () {
+		gameCanvas.context.fillStyle = color;
+		gameCanvas.context.fillRect(this.x, this.y, this.width, this.height);
+	};
+
+	this.move = function () {
+		if (this.pointer < this.bezierPoints.length) {
+			this.x = this.bezierPoints[this.pointer].x;
+			this.y = this.bezierPoints[this.pointer].y;
+			this.pointer++;
+		}
+	};
+};
+
+Bullet.prototype = Object.create(Entity.prototype);
+Bullet.prototype.constructor = Bullet;
+
+
