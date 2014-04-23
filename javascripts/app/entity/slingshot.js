2004a1dafc31a4769de607e98c0322d0785159f7
Srdjan Stojcic <srdjan206@hotmail.com>
2014-04-23 00:04:28 -0700

    fixed rotations

--- javascripts/app/entity/slingshot.js
+++ javascripts/app/entity/slingshot.js
@@ -22,16 +22,22 @@ var Slingshot = function (x, y, width, height) {
 		var distance = position.distanceTo(this.position);
 		var y = position.y - this.position.y;
 
-		this.angle = Math.acos(y / distance);
+		if (position.x > this.position.x) {
+			this.angle = Math.asin(y / distance);
+		} else {
+			this.angle = Math.acos(y / distance);
+		}
 
+		this.angle += 1.57079633;
+		/*
 		if (position.x > this.position.x && y > 0) {
 			this.angle += 1.57079633;
 		} else if (position.x < this.position.x && y < 0) {
 			this.angle += 1.57079633;
 		} else if (position.x < this.position.x && y > 0) {
-			this.angle += 1.57079633;
+			
 		}
-
+		*/
 	}
 
 };
