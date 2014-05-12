11d23e45ceb45a716597365fad07ea4d9b94f0fc
Srdjan Stojcic <srdjan206@hotmail.com>
2014-05-11 21:17:48 -0700

    some minor changes

--- javascripts/app/map/map.js
+++ javascripts/app/map/map.js
@@ -1,18 +1,9 @@
 var Map = function (canvas) {
 	
-	this.canvas = canvas;
-
-	this.scale = 30;
-
-	this.tileWidth = this.canvas.width / this.scale;
-	this.tileHeight = this.canvas.height / this.scale;
-
 	this.tiles = [];
-
 	this.controlPoints = [];
 	this.path = [];
 	
-
 	function loadJSON(path, success, error)
 	{
 	    var xhr = new XMLHttpRequest();
@@ -34,23 +25,27 @@ var Map = function (canvas) {
 	
 	var that = this;
 	
-	loadJSON('assets/maps/map1.json',
-	        function(data) { 
-				
-				for (var i = 0; i < data.length; i++) {
-					var record = data[i];
-					var tile = new Tile("#926239", record.x, record.y, record.width, record.height);
+	function loadMap(level) {
+		var mapToLoad = "assets/maps/map" + level + ".json";
+		
+		loadJSON(mapToLoad,
+		        function(data) { 
 					
-					that.tiles.push(tile);
-					that.controlPoints.push(tile.getCenter());
-					
-					if (that.controlPoints.length > 3) {
-						that.path = Bezier.calculateCurve(that.controlPoints);
+					for (var i = 0; i < data.length; i++) {
+						var record = data[i];
+						var tile = new Tile("#926239", record.x, record.y, record.width, record.height);
+						
+						that.tiles.push(tile);
+						that.controlPoints.push(tile.getCenter());
+						
+						if (that.controlPoints.length > 3) {
+							that.path = Bezier.calculateCurve(that.controlPoints);
+						}
 					}
-				}
-				
-				//console.log(data); 
-			},
-	        function(xhr) { console.error(xhr); }
-	);
+					
+					//console.log(data); 
+				},
+		        function(xhr) { console.error(xhr); }
+		);
+	}		
 };	
