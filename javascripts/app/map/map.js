1a27f181d62214090477b1e3d3ae8fb82056be8e
Srdjan Stojcic <srdjan206@hotmail.com>
2014-05-11 20:50:59 -0700

    added tiles and path to map

--- javascripts/app/map/map.js
+++ javascripts/app/map/map.js
@@ -1,5 +1,5 @@
-//var Map = function (canvas) {
-	/*
+var Map = function (canvas) {
+	
 	this.canvas = canvas;
 
 	this.scale = 30;
@@ -11,36 +11,46 @@
 
 	this.controlPoints = [];
 	this.path = [];
-	*/
+	
 
-function loadJSON(path, success, error)
-{
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
-}
-
-loadJSON('assets/maps/map1.json',
-        function(data) { 
-			
-			for (var i = 0; i < data.length; i++) {
-				var record = data[i];
-				console.log(record.x + ", " + record.y);
-			}
-			
-			//console.log(data); 
-		},
-        function(xhr) { console.error(xhr); }
-);
+	function loadJSON(path, success, error)
+	{
+	    var xhr = new XMLHttpRequest();
+	    xhr.onreadystatechange = function()
+	    {
+	        if (xhr.readyState === 4) {
+	            if (xhr.status === 200) {
+	                if (success)
+	                    success(JSON.parse(xhr.responseText));
+	            } else {
+	                if (error)
+	                    error(xhr);
+	            }
+	        }
+	    };
+	    xhr.open("GET", path, true);
+	    xhr.send();
+	}
+	
+	var that = this;
+	
+	loadJSON('assets/maps/map1.json',
+	        function(data) { 
+				
+				for (var i = 0; i < data.length; i++) {
+					var record = data[i];
+					var tile = new Tile("#926239", record.x, record.y, record.width, record.height);
+					
+					that.tiles.push(tile);
+					that.controlPoints.push(tile.getCenter());
+					
+					if (that.controlPoints.length > 3) {
+						that.path = Bezier.calculateCurve(that.controlPoints);
+					}
+				}
+				
+				//console.log(data); 
+			},
+	        function(xhr) { console.error(xhr); }
+	);
+};	
