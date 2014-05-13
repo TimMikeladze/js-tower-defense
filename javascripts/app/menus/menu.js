bc6a7349236c0dd4659c34c7c7862b6ef97903a2
cjcastil <cjcastil@uw.edu>
2014-05-12 23:29:50 -0700

    delete old menu folder

--- javascripts/app/menus/menu.js
+++ javascripts/app/menus/menu.js
@@ -1,3 +1,4 @@
+// not done
 var Menu = function (path, width, height) {
 	this.path = path;
 	this.width = width;
@@ -8,10 +9,17 @@ var Menu = function (path, width, height) {
 	this.x2 = null;
 	this.y2 = null;
 
-	this.setCoordinates = function(x1, y1, x2, y2) {
+	this.buttons = {};
+	var buttonIndex = 0;
+
+	this.setCoordinates = function (x1, y1, x2, y2) {
 		this.x1 = x1;
 		this.y1 = y1;
 		this.x2 = x2;
 		this.y2 = y2;
+	}
+
+	this.addButtonCoords = function (name, x1, y1, x2, y2) {
+		buttons.push(name, {x1: x1, y1: y1, x2: x2, y2: y2});
 	}
 }
