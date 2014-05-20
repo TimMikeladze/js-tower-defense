d4976bf4368b8e145095882482fb49f30721ba9a
cjcastil <cjcastil@uw.edu>
2014-05-19 21:55:02 -0700

    temp change

--- javascripts/app/menus/menu.js
+++ javascripts/app/menus/menu.js
@@ -1,7 +1,8 @@
 // not done
-/*
-var Menu = function (path, x, y, width, height) {
-	this.path = path;
+
+var Menu = function (canvas, path, x, y, width, height) {
+	this.canvas = canvas;
+	this.image = Require.getImage(path);
 	this.width = width;
 	this.height = height;
 
@@ -10,8 +11,11 @@ var Menu = function (path, x, y, width, height) {
 	this.buttons = {};
 	var buttonIndex = 0;
 
+	this.start() {
+		canvas.context.drawImage(Require.getImage(path), this.x, this.y);
+	}
+
 	this.addButtonCoords = function (name, x, y, w, h) {
-	//	buttons.push(name, {x: x, y: y, width: w, height: h});
+		buttons.push(name, {x: x, y: y, width: w, height: h});
 	}
 } 
-*/
