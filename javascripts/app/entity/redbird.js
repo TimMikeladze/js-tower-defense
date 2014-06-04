cb9739cd8aae2f4f0fa6b22560f7e0a0d823972c
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-06-03 23:37:29 -0700

    fixes

--- javascripts/app/entity/redbird.js
+++ javascripts/app/entity/redbird.js
@@ -30,7 +30,6 @@ var RedBird = function (position) {
 		projectile.setMaxFlightDistance(this.fireRadius);
 		projectile.fire();
 		engine.addProjectile(projectile);
-		sounds.play("firing");
 	};
 };
 
