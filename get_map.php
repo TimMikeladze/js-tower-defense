5f0e3f117d15c9962351e6189bb706fac24aa1f4
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-06-05 04:04:51 -0400

    fixed sql

--- get_map.php
+++ get_map.php
@@ -6,11 +6,9 @@ $Level = (int) $_GET['level'];
 $LevelSQL = "";
 if (!empty($Level)) {
 	$LevelSQL = " WHERE Level = '$Level' ";
-} else {
-	$LevelSQL = " ORDER BY RAND() ";
 }
 
-G::$DB->query("SELECT Name, Level, Data FROM maps $LevelSQL");;
+G::$DB->query("SELECT Name, Level, Data FROM maps $LevelSQL ORDER BY RAND() LIMIT 1");;
 
 if (G::$DB->record_count()) {
 	$Results = G::$DB->to_array(false, MYSQLI_ASSOC);
