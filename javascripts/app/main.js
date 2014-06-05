dad53f78cacdf7dc272cde4abeb14ea4730aabe8
Srdjan Stojcic <srdjan206@hotmail.com>
2014-06-04 18:07:17 -0700

    added helmet pig, updated readme

--- javascripts/app/main.js
+++ javascripts/app/main.js
@@ -20,7 +20,7 @@ Require.addScript("map", ["map", "tile"]);
 Require.addScript("menus", ["menubutton", "sidebar"]);
 Require.addScript("engine", ["time", "collisions", "funds", "gameengine", "input", "enemyqueue"]);
 Require.addScript("graphics", ["canvas", "animation", "animator"]);
-Require.addScript("entity", ["entity", "bird", "pig", "redbird", "greenpig", "projectile", "redbirdprojectile", "mustachepig", "kingpig"]);
+Require.addScript("entity", ["entity", "bird", "pig", "redbird", "greenpig", "projectile", "redbirdprojectile", "mustachepig", "kingpig", "helmetpig"]);
 
 Require.addImage("pigs", ["pig0.png"]);
 Require.addImage("pigs", ["pig1.png"]);
