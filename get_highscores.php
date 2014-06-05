c431bcd9ce0c5ccbabbc9df1d91d94eee9bcfa51
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-06-05 02:05:27 -0400

    add limit to get highscores

--- get_highscores.php
+++ get_highscores.php
@@ -2,7 +2,13 @@
 header('Access-Control-Allow-Origin: *'); 
 require("init.php");
 
-G::$DB->query("SELECT ID, Name, Score FROM highscores ORDER BY Score DESC");
+$Limit = (int) $_GET['limit'];
+$LimitSQL = "";
+if (!empty($Limit)) {
+    $LimitSQL = " LIMIT $Limit ";
+}
+
+G::$DB->query("SELECT ID, Name, Score FROM highscores ORDER BY Score DESC $LimitSQL");
 $Results = G::$DB->to_array(false, MYSQLI_ASSOC);
 
 echo json_encode($Results);
